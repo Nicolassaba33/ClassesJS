bca0d254710cf1d2b4cdb870783f8102ed14b8fa
Nicolassaba33 <nicolassaba33@gmail.com>
2023-07-13 20:59:15 -0300

    Se incorporo la clase 3

---
+++ Class 1/main.js
@@ -0,0 +1,42 @@
+//Comentario de linea
+
+/*Comentario de bloque
+tiene la misma funcionalidad*/
+
+//Recomendado para declarar variables
+//let edad = 10;
+//let nombre = "Andres";
+
+//No recomendada para declararar variables
+//var cantidad = 6;
+
+//Constantes que no varian su numero y se escribe todo en mayuscula
+//const NUMERO = 5;
+
+/*let numeroUno = 5;
+let numeroDos = 6;
+
+let resultadoSuma = numeroUno + numeroDos;
+let resultadoResta = numeroUno - numeroDos;
+let resultadoMultiplicacion = numeroUno * numeroDos;
+let resultadoDivision = numeroUno / numeroDos;
+console.log(resultadoSuma);
+console.log(resultadoResta);
+console.log(resultadoMultiplicacion);
+console.log(resultadoDivision);
+*/
+
+/*
+let saludo = "Hola";
+let nombre = "Nicolas";
+
+let mensajeCompleto = saludo +" "+nombre;
+
+console.log(mensajeCompleto);
+*/
+
+// let numero = Number(prompt("Ingrese un numero"));
+// alert(numero + 7);
+
+let nombre = prompt("Ingresa tu Nombre");
+alert("Hola" + " " + nombre + " " + "Bienvenido");
---
+++ Class 3/main.js
@@ -0,0 +1,120 @@
+/*
+
+Estructura del bucle for
+
+for (desde; hasta; actualizacion){
+    codigo a repetir
+}*/
+
+/*
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
+*/
+/*
+let numero = parseInt(prompt("Ingrese el numero"));
+
+for (let i = 1; i <= 10; i++) {
+  let resultado = numero * i;
+
+  alert(`${numero} * ${i} = ${resultado}`);
+}*/
+
+/*
+let edad = 10;
+console.log(`Hola mi nombre es ${edad}`);
+*/
+/*
+for (let turno = 1; turno <= 7; turno++) {
+  let nombre = prompt("Ingrese el nombre");
+  alert(`Turno #${turno}, Nombre: ${nombre}`);
+}
+*/
+
+/*ESTRUCTURA WHILE*/
+/*
+while (condicion){
+    codigo a repetir cuando la condicion es verdadera
+}
+
+*/
+
+/*let usuario = prompt("Ingrese el usuario");
+
+while (usuario != "andres") {
+  alert("Usuario incorrecto");
+  usuario = prompt("Ingrese el usuario");
+}
+*/
+
+/*do{
+    codigo a ejecutar por primera vez y cuando la condicion sea verdadera
+}
+while (condicion);*/
+
+// let repetir = false;
+
+// while (repetir){
+//     console.log("Entramos");
+// }
+
+// do {
+//   console.log("Entramos");
+// } while (repetir);
+
+// Estructura del Switch
+
+/*
+switch(valor){
+    case valor1:
+        codigo a ejecutar cuando el valor === valor1;
+        break;
+
+    case valor2:
+        codigo a ejecutar cuando el valor === valor2;
+        break;
+    
+    default:
+        codigo a ejecutar cuando valor no coincide con ningun valor
+        break;
+}
+*/
+
+/*
+let moneda = "ars";
+
+switch (moneda) {
+  case "ars":
+    console.log("Moneda de Argentina");
+    break;
+
+  case "cop":
+    console.log("Moneda de Colombia");
+    break;
+
+  case "clp":
+    console.log("Moneda de Chile");
+    break;
+
+  default:
+    console.log("Moneda no reconocida");
+    break;
+}
+*/
+
+/*
+let numerito = parseInt(prompt("Ingrese un numero"));
+
+for (i = 1; i < 10; i++) {
+  let resultado = numerito + i;
+  console.log(resultado);
+}
+
+let texto = prompt("Ingrese un texto");
+while (texto != "ESC") {
+  console.log(texto);
+}
+
+numerito = parseInt(prompt("Ingrese un numero"));
+
+*/
